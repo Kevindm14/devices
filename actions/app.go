29593f8082bcf999cfa05a3d205ee7e1f284869e
Kevin Diaz <kevindiaz@Kevins-MacBook-Pro.local>
2020-09-06 17:55:31 -0500

    ui-devices

--- actions/app.go
+++ actions/app.go
@@ -12,7 +12,6 @@ import (
 	"github.com/gobuffalo/buffalo-pop/v2/pop/popmw"
 	csrf "github.com/gobuffalo/mw-csrf"
 	i18n "github.com/gobuffalo/mw-i18n"
-	"github.com/gobuffalo/packr/v2"
 )
 
 // ENV is used to help switch settings based on where the
@@ -56,27 +55,13 @@ func App() *buffalo.App {
 		// Remove to disable this.
 		app.Use(popmw.Transaction(models.DB))
 
-		// Setup and use translations:
-		app.Use(translations())
-
 		app.GET("/", HomeHandler)
+		app.GET("/newDevice", newDeviceHandler)
 
 		app.ServeFiles("/", assetsBox) // serve files from the public directory
 	}
 
 	return app
-}
-
-// translations will load locale files, set up the translator `actions.T`,
-// and will return a middleware to use to load the correct locale for each
-// request.
-// for more information: https://gobuffalo.io/en/docs/localization
-func translations() buffalo.MiddlewareFunc {
-	var err error
-	if T, err = i18n.New(packr.New("app:locales", "../locales"), "en-US"); err != nil {
-		app.Stop(err)
-	}
-	return T.Middleware()
 }
 
 // forceSSL will return a middleware that will redirect an incoming request
